666545cec673b2116177893ca3d5bf7ff2bd97f7
Swayam Pendgaonkar <100400706+starrylight90@users.noreply.github.com>
2024-04-17 11:44:41 +0530

    CHanges to frontend and readme

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import createEvent from '../controllers/eventController.js';
+import {createEvent, displayEvents} from '../controllers/eventController.js';
 
 
 const eventRouter = express.Router();
 
-eventRouter.post('/event', createEvent);
+eventRouter.post('/event',createEvent);
+eventRouter.get('/displayevents', displayEvents);
 
 export default eventRouter;
